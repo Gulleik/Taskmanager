d163827f5f80af31e202baa8a6e41ab10153e563
Gulleik <gulleik.olsen@gmail.com>
2019-07-25 11:10:00 +0200

    =hashing lagt til

---
+++ src/routers/task.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const Task = require("../models/task")
+const router = new express.Router()
+
+router.get("/tasks", async (req,res)=>{
+    try{
+        const task = await Task.find({})
+        res.status(200).send(tasks)
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+router.get("/tasks/:id", async (req,res)=>{
+    const _id = req.params.id;
+
+
+    try{
+        const task = await Task.findById(_id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.post("/tasks", async (req, res) =>{
+    const task = new Task(req.body)
+
+    try{
+        const task = await task.save()
+        res.status(201).send(task);
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.patch("/tasks/:id", async(req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description", "completed"]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: "ugyldig oppdatering"})
+    }
+
+    try{
+        const task = await Task.findById(req.params.id)
+        updates.forEach((update)=>task[update] = req.body[update])
+        task.save()
+        
+        if(!task){
+            return res.status(404).send() 
+        }
+        res.status(200).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.delete("/tasks/:id",async (req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            res.status(404).send({error: "no task by that id"})
+        }
+
+        res.status(200).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+module.exports = router
