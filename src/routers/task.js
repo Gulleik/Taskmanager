1771e3f9acdaac0353c8910c33ce87aecbd4e5ee
Gulleik <gulleik.olsen@gmail.com>
2019-08-12 12:58:47 +0200

    =authorization finished

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,9 +3,9 @@ const Task = require("../models/task")
 const auth = require("../middleware/Authentication")
 const router = new express.Router()
 
-router.get("/tasks", async (req,res)=>{
+router.get("/tasks", auth, async (req,res)=>{
     try{
-        const task = await Task.find({})
+        const tasks = await Task.find({owner: req.user._id})
         res.status(200).send(tasks)
     }catch(e){
         res.status(500).send()
@@ -41,7 +41,7 @@ router.post("/tasks", auth, async (req, res) =>{
     }
 })
 
-router.patch("/tasks/:id", async(req,res)=>{
+router.patch("/tasks/:id", auth, async(req,res)=>{
     const updates = Object.keys(req.body)
     const allowedUpdates = ["description", "completed"]
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -51,22 +51,22 @@ router.patch("/tasks/:id", async(req,res)=>{
     }
 
     try{
-        const task = await Task.findById(req.params.id)
-        updates.forEach((update)=>task[update] = req.body[update])
-        task.save()
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
         
         if(!task){
             return res.status(404).send() 
         }
+        updates.forEach((update)=>task[update] = req.body[update])
+        task.save()
         res.status(200).send(task)
     }catch(e){
         res.status(400).send(e)
     }
 })
 
-router.delete("/tasks/:id",async (req,res)=>{
+router.delete("/tasks/:id", auth, async (req,res)=>{
     try{
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user._id})
         if(!task){
             res.status(404).send({error: "no task by that id"})
         }
